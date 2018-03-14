f46681fba282cf9e1d7fdde9f0c4ef146d74cb8c
dkolarova <dkolarova@CSCBGRAH753702.obs.local>
2018-03-14 16:07:58 +0200

    Implementation refined, test extended.

--- src/main/java/edu/examples/FileWordIterator.java
+++ src/main/java/edu/examples/FileWordIterator.java
@@ -2,30 +2,119 @@ package edu.examples;
 
 import java.io.BufferedReader;
 import java.io.Closeable;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.Reader;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 import java.util.Objects;
 
-public class FileWordIterator implements Iterator<String>, Closeable {
+/**
+ * File based iterator which parses a file and breaks it into words using
+ * whitespace or a sequence of whitespace characters as delimiter. It expects a
+ * file name as an input. Passing a <code>null</code> parameter into the
+ * constructor of the <code>FileWordIterator</code> will cause a
+ * <code>NullPointerException</code> to be thrown.
+ * 
+ * Example<br/>
+ * The text:<br/>
+ * <b>HitchHiker's[Space][Space]Guide[Tab][Space]to[Space]the[Space]Galaxy[Space]Intro[NewLine]</b>
+ * will be broken into words using the iterator the following way:<br/>
+ * <ul>
+ * <li>HitchHiker's</li>
+ * <li>Guide</li>
+ * <li>to</li>
+ * <li>the</li>
+ * <li>Galaxy</li>
+ * <li>Intro</li>
+ * </ul>
+ * 
+ * Note that if a word is spread over two lines using dash symbol '-' it will be
+ * interpreted by the iterator as two separate words due to the whitespace
+ * symbol (new line). This is a limitation of the class that has to be
+ * considered when using it.
+ * 
+ * @author dkolarova
+ *
+ */
+public final class FileWordIterator implements Iterator<String>, Closeable {
 
+	/**
+	 * Regular expression <code>\\s+</code> - matches sequence of one or more
+	 * whitespace characters.
+	 */
 	private static final String WHITE_SPACE_PATTERN = "\\s+";
 
+	/**
+	 * Wrapper class used for input source buffering. In general, each read
+	 * request made of a Reader causes a corresponding read request to be made
+	 * of the underlying character or byte stream. It is therefore advisable to
+	 * wrap a BufferedReader around any Reader whose read() operations may be
+	 * costly, such as FileReaders and InputStreamReaders. This is an
+	 * implementation detail of the class that might change in the future.
+	 */
 	private BufferedReader sourceReader;
 
+	/**
+	 * Buffer containing words/tokens that has been read form the underlying
+	 * source. Words are cached after each read operations.
+	 */
 	private String[] tokensBuffer;
 
+	/**
+	 * Pointer for the current position in the tokens buffer
+	 */
 	private int currentPosition;
 
+	/**
+	 * Boolean flag that holds information about the status of the iterator
+	 * implementing <code>Closable</code>.
+	 */
 	private boolean closed = false;
 
-	public FileWordIterator(InputStream source) {
-		Objects.requireNonNull(source, "source");
-		this.sourceReader = new BufferedReader(new InputStreamReader(source));
+	/**
+	 * Constructs a <code>FileWordIterator</code> that returns values scanned
+	 * from the specified source delimited by whitespace or a sequence of
+	 * whitespace characters.
+	 *
+	 * @param sourceFileName
+	 *            Name of the source file being iterated.
+	 * 
+	 *            <br/>
+	 *            Attempting to pass a <code>null</code> value for the input
+	 *            parameter <code>sourceFileName</code> will result in a
+	 *            {@link NullPointerException}.
+	 * 
+	 *            <br/>
+	 *            Attempting to pass a name of an non existing file will result
+	 *            in a {@link IllegalStateException}.
+	 */
+	public FileWordIterator(String sourceFileName) {
+		Objects.requireNonNull(sourceFileName, "Source file name");
+
+		Reader fileReader = null;
+		try {
+			fileReader = new FileReader(sourceFileName);
+		} catch (FileNotFoundException e) {
+			throw new IllegalStateException("Source file is invalid: " + e.getMessage(), e);
+		}
+
+		this.sourceReader = new BufferedReader(fileReader);
 	}
 
+	/**
+	 * Returns {@code true} if the the file contains more elements/words. (In
+	 * other words, returns {@code true} if {@link #next} would return an
+	 * element rather than throwing an exception.)
+	 * 
+	 * <br/>
+	 * Attempting to call method <code>hasNext</code> on a closed iterator will
+	 * result in a {@link IllegalStateException}.
+	 * 
+	 * @return {@code true} if the iteration has more elements/words
+	 * 
+	 */
 	@Override
 	public boolean hasNext() {
 		ensureReaderIsOpen();
@@ -36,10 +125,20 @@ public class FileWordIterator implements Iterator<String>, Closeable {
 		return hasWordInBuffer();
 	}
 
+	/**
+	 * Returns the next word in the iteration.
+	 * 
+	 * <br/>
+	 * Attempting to call method <code>hasNext</code> on a closed iterator will
+	 * result in a {@link IllegalStateException}.
+	 * 
+	 * @return the next word in the iteration
+	 * 
+	 * @throws NoSuchElementException
+	 *             if the iteration has no more elements
+	 */
 	@Override
 	public String next() {
-		ensureReaderIsOpen();
-
 		if (!hasNext()) {
 			throw new NoSuchElementException("No words to read");
 		}
@@ -51,20 +150,17 @@ public class FileWordIterator implements Iterator<String>, Closeable {
 	}
 
 	/**
-	 * Closes this scanner.
+	 * Closes the iterator.
 	 *
-	 * <p>
-	 * If this scanner has not yet been closed then if its underlying
-	 * {@linkplain java.lang.Readable readable} also implements the
-	 * {@link java.io.Closeable} interface then the readable's <tt>close</tt> method
-	 * will be invoked. If this scanner is already closed then invoking this method
-	 * will have no effect.
+	 * <br/>
+	 * If this scanner is already closed then invoking this method will have no
+	 * effect.
 	 *
-	 * <p>
-	 * Attempting to perform search operations after a scanner has been closed will
-	 * result in an {@link IllegalStateException}.
+	 * <br/>
+	 * Attempting to perform search operations after the iterator has been
+	 * closed will result in an {@link IllegalStateException}.
 	 * 
-	 * @throws IOException
+	 * @throws IOException ???
 	 *
 	 */
 	public void close() throws IOException {
--- src/test/java/edu/examples/FileWordIteratorTest.java
+++ src/test/java/edu/examples/FileWordIteratorTest.java
@@ -1,34 +1,33 @@
 package edu.examples;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 
 public class FileWordIteratorTest {
 
+	private static final String EMPTY_TEXT_FILE = "Empty.txt";
+
 	private static final String SIMPLE_TEXT_FILE = "HitchHiker.txt";
-	
+
+	private static final String SIMPLE_TEXT_FILE_EXACT_MATCH = "HitchHikerExactMatch.txt";
+
 	private static final String LARGE_TEXT_FILE = "HitchHikersGuide.txt";
 
 	private static final int SIMPLE_TEXT_FILE_WORDS_COUNT = 160;
-	
+
 	private static final int LARGE_TEXT_FILE_WORDS_COUNT = 47086;
-
-	@Before
-	public void setUp() {
-
-	}
 
 	@Test
 	public void testIterateSimpleData() {
 		int count = 0;
-		InputStream inputStream = FileWordIteratorTest.class.getResourceAsStream(SIMPLE_TEXT_FILE);
-		try (FileWordIterator fileWordIterator = new FileWordIterator(inputStream)) {
+		try (FileWordIterator fileWordIterator = new FileWordIterator(
+				FileWordIteratorTest.class.getResource(SIMPLE_TEXT_FILE).getFile())) {
 
 			while (fileWordIterator.hasNext()) {
 				fileWordIterator.next();
@@ -40,12 +39,12 @@ public class FileWordIteratorTest {
 
 		Assert.assertEquals(SIMPLE_TEXT_FILE_WORDS_COUNT, count);
 	}
-	
+
 	@Test
 	public void testIterateSimpleDataExactWordsMatch() {
 		List<String> words = new ArrayList<>();
-		InputStream inputStream = FileWordIteratorTest.class.getResourceAsStream(SIMPLE_TEXT_FILE);
-		try (FileWordIterator fileWordIterator = new FileWordIterator(inputStream)) {
+		try (FileWordIterator fileWordIterator = new FileWordIterator(
+				FileWordIteratorTest.class.getResource(SIMPLE_TEXT_FILE_EXACT_MATCH).getFile())) {
 
 			while (fileWordIterator.hasNext()) {
 				String word = fileWordIterator.next();
@@ -55,17 +54,60 @@ public class FileWordIteratorTest {
 			Assert.assertTrue(ioEx.getMessage(), false);
 		}
 
-		Assert.assertEquals(SIMPLE_TEXT_FILE_WORDS_COUNT, words.size());
+		List<String> expectedWordsList = loadFileRowsIntoList();
+
+		Assert.assertTrue(expectedWordsList.equals(words));
+	}
+	
+	@Test(expected = NoSuchElementException.class)
+	public void testIterateSimpleDataAfterEndReached() {
+		try (FileWordIterator fileWordIterator = new FileWordIterator(
+				FileWordIteratorTest.class.getResource(SIMPLE_TEXT_FILE).getFile())) {
+
+			while (fileWordIterator.hasNext()) {
+				fileWordIterator.next();
+			}
+			
+			fileWordIterator.next();
+		} catch (IOException ioEx) {
+			Assert.assertTrue(ioEx.getMessage(), false);
+		}
+	}
+
+	@Test(expected = IllegalStateException.class)
+	public void testHasNextSimpleDataAfterIteratorClosed() {
+		try (FileWordIterator fileWordIterator = new FileWordIterator(
+				FileWordIteratorTest.class.getResource(SIMPLE_TEXT_FILE).getFile())) {
+
+			fileWordIterator.close();
+			fileWordIterator.hasNext();
+		} catch (IOException ioEx) {
+			Assert.assertTrue(ioEx.getMessage(), false);
+		}
+	}
+	
+	@Test(expected = IllegalStateException.class)
+	public void testIterateSimpleDataAfterIteratorClosed() {
+		try (FileWordIterator fileWordIterator = new FileWordIterator(
+				FileWordIteratorTest.class.getResource(SIMPLE_TEXT_FILE_EXACT_MATCH).getFile())) {
+
+			while (fileWordIterator.hasNext()) {
+				fileWordIterator.close();
+				fileWordIterator.next();
+			}
+		} catch (IOException ioEx) {
+			Assert.assertTrue(ioEx.getMessage(), false);
+		}
 	}
 
 	@Test
 	public void testIterateLargeData() {
 		int count = 0;
-		InputStream inputStream = FileWordIteratorTest.class.getResourceAsStream(LARGE_TEXT_FILE);
-		try (FileWordIterator fileWordIterator = new FileWordIterator(inputStream)) {
+		try (FileWordIterator fileWordIterator = new FileWordIterator(
+				FileWordIteratorTest.class.getResource(LARGE_TEXT_FILE).getFile())) {
 
 			while (fileWordIterator.hasNext()) {
-				System.out.println(fileWordIterator.next());
+				fileWordIterator.next();
 				count++;
 			}
 		} catch (IOException ioEx) {
@@ -74,12 +116,12 @@ public class FileWordIteratorTest {
 
 		Assert.assertEquals(LARGE_TEXT_FILE_WORDS_COUNT, count);
 	}
-	
+
 	@Test(expected = NullPointerException.class)
-	public void testIterateNonExistingFile() {
+	public void testIterateOnNonExistingFile() {
 		int count = 0;
-		InputStream inputStream = FileWordIteratorTest.class.getResourceAsStream("someFile8789.txt");
-		try (FileWordIterator fileWordIterator = new FileWordIterator(inputStream)) {
+		try (FileWordIterator fileWordIterator = new FileWordIterator(
+				FileWordIteratorTest.class.getResource("someFile8789.txt").getFile())) {
 
 			while (fileWordIterator.hasNext()) {
 				fileWordIterator.next();
@@ -91,7 +133,37 @@ public class FileWordIteratorTest {
 
 		Assert.assertEquals(SIMPLE_TEXT_FILE_WORDS_COUNT, count);
 	}
+	
+	@Test
+	public void testHasNextOnEmptyFile() {
+		try (FileWordIterator fileWordIterator = new FileWordIterator(
+				FileWordIteratorTest.class.getResource(EMPTY_TEXT_FILE).getFile())) {
+
+			Assert.assertFalse(fileWordIterator.hasNext());
+		} catch (IOException ioEx) {
+			Assert.assertTrue(ioEx.getMessage(), false);
+		}
+	}
+	
+	@Test(expected = NoSuchElementException.class)
+	public void testIterateOnEmptyFile() {
+		try (FileWordIterator fileWordIterator = new FileWordIterator(
+				FileWordIteratorTest.class.getResource(EMPTY_TEXT_FILE).getFile())) {
+
+			fileWordIterator.next();
+		} catch (IOException ioEx) {
+			Assert.assertTrue(ioEx.getMessage(), false);
+		}
+	}
 
 
+	private List<String> loadFileRowsIntoList() {
+		List<String> words = new ArrayList<>();
+		words.addAll(Arrays.asList("HitchHiker's", "Guide", "to", "the", "Galaxy", "Intro", "Far", "out", "in", "the",
+				"uncharted", "backwaters", "of", "the", "unfashionable", "end", "of", "the", "western", "spiral", "arm",
+				"of", "the", "Galaxy", "lies", "a", "small", "unregarded", "yellow", "sun."));
+
+		return words;
+	}
 
 }
